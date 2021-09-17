c303440fee31f792b15c6b3f146b528d8ad855c0
Laith <laith.marzouka@gmail.com>
2021-09-18 00:50:23 +0300

    improved and integrated the greeting system

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,7 +7,19 @@ import { Category, Menu } from './menu.model';
 export class AppService {
   constructor(@InjectModel('Menu') private readonly menuModel: Model<Menu>) {}
 
-  welcome = [
+  NAME = [
+    'Mr Robert',
+    'Mr Rober',
+    'Mr Roberto',
+    'Mr.Rober',
+    'Mr.Roberto',
+    'Mr.Robert',
+    'Robert',
+    'Roberto',
+    'Rober',
+  ];
+
+  WELCOME = [
     'Hello',
     'Hey',
     'Heya',
@@ -19,24 +31,86 @@ export class AppService {
     'Good evening',
   ];
 
+  MOOD = [
+    'How are you',
+    'How is everything',
+    'How are things',
+    'Is everything okay',
+    'Are you okay',
+    'Are you fine',
+    'Is everything fine',
+    'what is going on',
+  ];
+
+  SuperFeelings = [
+    `is Lit`,
+    `is super`,
+    `is extraordinary`,
+    `is feeling amazing`,
+    `is feeling super duper`,
+    `is feeling marvelous`,
+  ];
+
+  GoodFeelings = [`is good`, `is fine`, `is feeling good`, `is happy`];
+
+  NeutralFeelings = [`is okay`, `is not bad`];
+
+  BadFeelings = [
+    `is feeling bad`,
+    `is feeling sick`,
+    `is not okay`,
+    `had better days`,
+    `is feeling terrible`,
+    `had a bad day`,
+  ];
+
   request(message: string): { response: string } {
     let response = '';
     message = message.toLowerCase().trim();
-    // let query = '';
-    const greeting = this.contain(message, this.welcome);
+
+    // Checking for Name & Greetings & How are you & if message is empty!
+    const name = this.contains(message, this.NAME);
+    const greeting = this.contains(message, this.WELCOME);
+    const mood = this.contains(message, this.MOOD);
+    console.log(name, greeting, mood);
     if (greeting) {
-      response = greeting + ' :)';
+      response += greeting;
+      if (!name && !mood) {
+        response += ', Robert at your service, ask me what can I do!';
+      }
+    }
+    if (name || !message.replace(/\s/g, '').length) {
+      if (greeting) {
+        response += ', ' + name;
+      } else {
+        response += name;
+      }
+      if (mood) {
+        response += ' ' + this.randomFeeling();
+      } else {
+        response += ' at your service, ask me what can I do!';
+      }
+    }
+    if (mood && !name) {
+      if (greeting) {
+        response += ', Robert ' + this.randomFeeling();
+      } else {
+        response = 'Robert ' + this.randomFeeling();
+      }
     }
     if (!response) {
-      response = "Sorry I didn't understand that, I'm still learning :)";
+      response = `Sorry I don't know, I'm still learning :)`;
     }
     return { response };
   }
-  private contain = (s: string, array: string[]): string => {
+
+  private contains = (s: string, array: string[]): string => {
     for (let i = 0; i < array.length; i++) {
-      const word = array[i].toLowerCase();
-      const reg = '^' + word + '(?:\\s|[!?]*|$)';
-      const regex = new RegExp(reg, 'i');
+      const name = array[i].toLowerCase();
+      const regex = new RegExp(
+        '(?:^|[\\s.,?!])' + name + '(?:$|[\\s.,?!])',
+        'i',
+      );
       const match = s.match(regex);
       if (match !== null) {
         return array[i];
@@ -44,6 +118,37 @@ export class AppService {
     }
     return null;
   };
+
+  private randomFeeling(): string {
+    const hour = new Date().getHours();
+    let luck;
+    if (hour > 19 || hour <= 6) {
+      luck = 5;
+    } else if (hour > 6 || hour <= 11) {
+      luck = 3;
+    } else if (hour > 11 || hour <= 19) {
+      luck = 1;
+    }
+    const random = Math.random();
+    const randomNumber = Math.floor(random * 10 * luck);
+    if (randomNumber > 46) {
+      return this.SuperFeelings[
+        Math.floor(Math.random() * this.SuperFeelings.length)
+      ];
+    } else if (randomNumber > 20) {
+      return this.GoodFeelings[
+        Math.floor(Math.random() * this.GoodFeelings.length)
+      ];
+    } else if (randomNumber > 5) {
+      return this.NeutralFeelings[
+        Math.floor(Math.random() * this.NeutralFeelings.length)
+      ];
+    } else {
+      return this.BadFeelings[
+        Math.floor(Math.random() * this.BadFeelings.length)
+      ];
+    }
+  }
 
   async getMenu(): Promise<Menu[]> {
     const menu = await this.menuModel.find();
